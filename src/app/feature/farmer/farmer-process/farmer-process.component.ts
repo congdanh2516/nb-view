17c83e424f5c0b2ff550d082b8973ff2f865db24
lenhuthao1908 <nhuthaole1908@gmail.com>
2023-11-20 21:39:11 +0700

    Create process detail notebook

--- src/app/feature/farmer/farmer-process/farmer-process.component.ts
+++ src/app/feature/farmer/farmer-process/farmer-process.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-farmer-process',
   templateUrl: './farmer-process.component.html',
-  styleUrls: ['./farmer-process.component.scss']
+  styleUrls: ['./farmer-process.component.scss'],
 })
-export class FarmerProcessComponent {
-
-}
+export class FarmerProcessComponent {}

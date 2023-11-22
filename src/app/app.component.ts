126f1254698e221e716de4b536ce4b6cb5e6cdc9
congdanh.2516 <congdanh.2516.jk@gmail.com>
2023-11-22 16:41:02 +0700

    processing confirm-box-modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { ConfirmBoxModalService } from './core/services/confirm-box-modal.service';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,19 @@ export class AppComponent {
 
   faCircleUser = faCircleUser;
   panelOpenState = false;
+
+  confirm: {
+    isDisplay: boolean,
+    message: string,
+    isLoading: boolean,
+    isCancel: boolean
+  } = {isDisplay: true, message: 'abc', isLoading : false, isCancel: false}
+
+  constructor(private confirmSV: ConfirmBoxModalService) {
+    this.confirmSV.getMessage().subscribe((data) => {
+      console.log("confirmmm,m: ", data);
+      // this.confirm.isDisplay=data.isDisplay;
+    });
+    
+  }
 }

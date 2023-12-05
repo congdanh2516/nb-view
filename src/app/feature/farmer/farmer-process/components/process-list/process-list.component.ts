a0af24c2448511e7b3072e190f41f0f780102711
lenhuthao1908 <nhuthaole1908@gmail.com>
2023-12-05 11:46:47 +0700

    design ui

--- src/app/feature/farmer/farmer-process/components/process-list/process-list.component.ts
+++ src/app/feature/farmer/farmer-process/components/process-list/process-list.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { ConfirmBoxModalService } from 'src/app/core/services/confirm-box-modal.service';
+import { ProjectService } from 'src/app/core/services/project/project.service';
 
 @Component({
   selector: 'app-process-list',
   templateUrl: './process-list.component.html',
-  styleUrls: ['./process-list.component.scss']
+  styleUrls: ['./process-list.component.scss'],
 })
 export class ProcessListComponent {
   faPlus = faPlus;
-  personalProcessBool : Boolean = false; //true: co quy trinh, false: chua co quy trinh
+  personalProcessBool: Boolean = false; //true: co quy trinh, false: chua co quy trinh
+  projectList: Array<any> = [];
+  projectListByCreatorId: Array<any> = [];
 
-  constructor(private confirmSV: ConfirmBoxModalService) {
+  constructor(
+    private confirmSV: ConfirmBoxModalService,
+    private projectSV: ProjectService
+  ) {
     this.confirmSV.getMessage().subscribe((data) => {
-      console.log("data: ", data);
-      if(data.action=='close') {
-        if(data.acceptance) {
+      console.log('data: ', data);
+      if (data.action == 'close') {
+        if (data.acceptance) {
           this.addNewProcess2();
         }
       }
-    })
+    });
   }
 
   addNewProcess() {
@@ -27,12 +33,34 @@ export class ProcessListComponent {
       isDisplay: true,
       message: {
         title: 'abc',
-        description: 'xyz'
-      }
-    })
+        description: 'xyz',
+      },
+    });
   }
 
   addNewProcess2() {
-    console.log("Add new process");
+    console.log('Add new process');
+  }
+
+  getAllProject() {
+    this.projectSV.getAllProject().subscribe({
+      next: (data: any) => {
+        this.projectList = data;
+      },
+      error: () => {
+        console.log('error: getAllProject');
+      },
+    });
+  }
+
+  getAllProjectByCreatorId(creatorId: any) {
+    this.projectSV.getAllProjectByCreatorId(creatorId).subscribe({
+      next: (data: any) => {
+        this.projectListByCreatorId = data;
+      },
+      error: () => {
+        console.log('error: getAllProjectByCreatorId');
+      },
+    });
   }
 }
